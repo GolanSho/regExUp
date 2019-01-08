53fbd25b2c30b6fc2993b58600899696eede0fe4
GolanSho <xccxxxccx@gmail.com>
2019-01-08 07:10:47 -0500

    new script

---
+++ newusers.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+######################################
+# Created By Golansho
+#
+# Date -
+#
+# Purpose : 
+#
+# Ver : 1.0.0
+######################################
+
+#set -x
+#######
+
+userslist="user1 user2 user3 user4 user5"
+
+f_useradd(){
+
+if
+for f in $userslist 
+do grep -q "$f" /etc/passwd 
+done       
+ then 
+   echo "user already exist" && exit
+        else 
+   echo "user is being created"
+  
+           fi
+
+   for f in $userslist
+do sudo useradd -m "$f"
+	done
+ 
+}
+#########
+#Main
+#########
+
+f_useradd
+
+
+
