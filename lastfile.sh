d1610132321b843776a7086a5301a46b1b0ebc7c
root <root@localhost.localdomain>
2019-01-10 11:55:11 -0500

    new scripts

---
+++ lastfile.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+######################################
+# Created By Golansho
+#
+# Date -
+#
+# Purpose : 
+#
+# Ver : 1.0.0
+######################################
+
+#set -x
+#######
+
+ls -lat /tmp |grep -n 
+
